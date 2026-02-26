5382f60c96f6ae471556f3b774840e65117a7a15
Michiel Lowijs <mlowijs@gmail.com>
2026-02-26 10:24:24 +0100

    good

--- prep.sh
+++ prep.sh
@@ -9,25 +9,22 @@ if [ $# -lt 1 ]; then
 fi
 
 SERIAL=$1
-OPWD=`pwd`
 BOOT=/mnt/tftpboot/boot/$SERIAL
 ROOT=/mnt/tftpboot/root/$SERIAL
 
 echo 'Creating directories...'
 sudo mkdir $BOOT $ROOT
 
-echo 'Copying root...'
-cd $OPWD
+echo 'Setting up root...'
 sudo tar xpf void-rpi*.tar.xz -C $ROOT
+sudo cp 10-root.conf $ROOT/etc/ssh/sshd_config.d
+sudo cp finalize.sh $ROOT/root
+echo 'WARNING: system not yet finalized!' | sudo tee $ROOT/etc/motd > /dev/null
 
-echo 'Copying boot...'
-sudo cp $ROOT/boot/kernel* $BOOT
+echo 'Setting up boot...'
+#sudo cp -R firmware/boot/*.dtb firmware/boot/fixup* firmware/boot/start* firmware/boot/overlays/ $BOOT
+sudo cp -pR $ROOT/boot/* $BOOT
 sudo rm -rf $ROOT/boot/*
-cd $OPWD/firmware/boot
-sudo cp -R *.dtb fixup* start* overlays/ $BOOT
-
-echo 'Configuring boot...'
-cd $OPWD
 sed -e "s/SERIALNO/$SERIAL/g" cmdline.txt | sudo tee $BOOT/cmdline.txt > /dev/null
 sudo cp config.txt $BOOT
 
@@ -35,18 +32,9 @@ echo 'Setting up fstab...'
 echo "$NFSHOST:/srv/netboot/boot/$SERIAL /boot nfs4 _netdev,noatime,proto=tcp,async 0 0" | sudo tee -a $ROOT/etc/fstab > /dev/null
 echo "$NFSHOST:/srv/netboot/root/$SERIAL / nfs4 _netdev,noatime,proto=tcp,async 0 0" | sudo tee -a $ROOT/etc/fstab > /dev/null
 
-echo 'Enabling SSH access for root...'
-cd $OPWD
-sudo cp 10-root.conf $ROOT/etc/ssh/sshd_config.d
-
 echo 'Setting repo mirror...'
 sudo mkdir -p $ROOT/etc/xbps.d
 sudo cp $ROOT/usr/share/xbps.d/*-repository-*.conf $ROOT/etc/xbps.d/
 sudo sed -i 's|https://repo-default.voidlinux.org|https://repo-de.voidlinux.org|g' $ROOT/etc/xbps.d/*-repository-*.conf
-
-echo 'Copying finalize script...'
-cd $OPWD
-sudo cp finalize.sh $ROOT/root
-echo 'WARNING: system not yet finalized!' | sudo tee $ROOT/etc/motd > /dev/null
 
 echo 'Done.'
