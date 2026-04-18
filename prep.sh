cd6ec59bf3f03b9af0840d181e975f73c238537f
Michiel Lowijs <mlowijs@gmail.com>
2026-04-19 00:08:08 +0200

    doas

--- prep.sh
+++ prep.sh
@@ -13,28 +13,28 @@ BOOT=/mnt/strmirr/netboot/boot/$SERIAL
 ROOT=/mnt/strmirr/netboot/root/$SERIAL
 
 echo 'Creating directories...'
-sudo mkdir $BOOT $ROOT
+doas mkdir $BOOT $ROOT
 
 echo 'Setting up root...'
-sudo tar xpf void-rpi*.tar.xz -C $ROOT
-sudo cp 10-root.conf $ROOT/etc/ssh/sshd_config.d
-sudo cp finalize.sh $ROOT/root
-echo 'WARNING: system not yet finalized!' | sudo tee $ROOT/etc/motd > /dev/null
+doas tar xpf void-rpi*.tar.xz -C $ROOT
+doas cp 10-root.conf $ROOT/etc/ssh/sshd_config.d
+doas cp finalize.sh $ROOT/root
+echo 'WARNING: system not yet finalized!' | doas tee $ROOT/etc/motd > /dev/null
 
 echo 'Setting up boot...'
-#sudo cp -R firmware/boot/*.dtb firmware/boot/fixup* firmware/boot/start* firmware/boot/overlays/ $BOOT
-sudo cp -pR $ROOT/boot/* $BOOT
-sudo rm -rf $ROOT/boot/*
-sed -e "s/SERIALNO/$SERIAL/g" cmdline.txt | sudo tee $BOOT/cmdline.txt > /dev/null
-sudo cp config.txt $BOOT
+#doas cp -R firmware/boot/*.dtb firmware/boot/fixup* firmware/boot/start* firmware/boot/overlays/ $BOOT
+doas cp -pR $ROOT/boot/* $BOOT
+doas rm -rf $ROOT/boot/*
+sed -e "s/SERIALNO/$SERIAL/g" cmdline.txt | doas tee $BOOT/cmdline.txt > /dev/null
+doas cp config.txt $BOOT
 
 echo 'Setting up fstab...'
-echo "$NFSHOST:/srv/netboot/boot/$SERIAL /boot nfs4 _netdev,noatime,proto=tcp,async 0 0" | sudo tee -a $ROOT/etc/fstab > /dev/null
-echo "$NFSHOST:/srv/netboot/root/$SERIAL / nfs4 _netdev,noatime,proto=tcp,async 0 0" | sudo tee -a $ROOT/etc/fstab > /dev/null
+echo "$NFSHOST:/srv/netboot/boot/$SERIAL /boot nfs4 _netdev,noatime,proto=tcp,async 0 0" | doas tee -a $ROOT/etc/fstab > /dev/null
+echo "$NFSHOST:/srv/netboot/root/$SERIAL / nfs4 _netdev,noatime,proto=tcp,async 0 0" | doas tee -a $ROOT/etc/fstab > /dev/null
 
 echo 'Setting repo mirror...'
-sudo mkdir -p $ROOT/etc/xbps.d
-sudo cp $ROOT/usr/share/xbps.d/*-repository-*.conf $ROOT/etc/xbps.d/
-sudo sed -i 's|https://repo-default.voidlinux.org|https://repo-de.voidlinux.org|g' $ROOT/etc/xbps.d/*-repository-*.conf
+doas mkdir -p $ROOT/etc/xbps.d
+doas cp $ROOT/usr/share/xbps.d/*-repository-*.conf $ROOT/etc/xbps.d/
+doas sed -i 's|https://repo-default.voidlinux.org|https://repo-de.voidlinux.org|g' $ROOT/etc/xbps.d/*-repository-*.conf
 
 echo 'Done.'
